4ff9a29b041d657c4b92fd241a3ab291e8476e3d
Javier Mercado-Peralta <JJMercadoPeralta@student.fullsail.edu>
2023-11-17 10:34:08 -0500

    In class finished

---
+++ config.py
@@ -0,0 +1,27 @@
+#this is our configuration folder to configure flask to our app location & variable needed to run Flask
+
+import os #operating system
+from dotenv import load_dotenv #allows us to load our environment varibales (variables needed to run application)
+
+
+#establish our base directory so whenever we use "." to referenece any location in our app it knows we are referencing rangers_shop folder
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#need to establish where our environment varibales are coming from (this file will be hidden from github)
+load_dotenv(os.path.join(basedir, '.env'))
+
+#create our Config class
+class Config():
+    """
+    Create Config class which will setup our configuration variables
+    Using environment varibales where available other create config variables.
+    """
+    #regular configuration for Flask App
+    FLASK_APP = os.environ.get('FLASK_APP') #looking for key of FLASK_APP in our environment variable location (.env)
+    FLASK_APP = os.environ.get("FLASK_ENV")
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
+    
+    #configuration if you are connecting a database
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Literally whatever you want as long as its a string. Cool Beans'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False #we dont want a message every single time our database is changed
